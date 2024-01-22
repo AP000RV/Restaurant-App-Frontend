d10dd69470d4654426e8dc9305ca012d78e7e506
AP000RV <apoorvjain1307@gmail.com>
2024-01-22 20:34:17 +0530

    error solve

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-import Home from '../Pages/Home';
-import Success from '../Pages/Success';
-import Notfound from '../Pages/Notfound';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Home from "../Pages/Home";
+import Success from "../Pages/Success.jsx";
+import Notfound from "../Pages/Notfound.jsx";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/success' element={<Success/>}/>
-        <Route path='*' element={<Notfound/>}/> 
+        <Route path="/" element={<Home />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="*" element={<Notfound />} />
       </Routes>
-      <Toaster/>
+      <Toaster />
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-
-// * means the path which is not defined will go to the notfound page...  
+// * means the path which is not defined will go to the notfound page...
